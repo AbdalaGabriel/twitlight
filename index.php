a3ce15b78b0248fc303047ed755ec64d11564efa
AbdalaGabriel <g.abdala.04@gmail.com>
2017-06-19 21:55:36 -0300

    v 0.0.0
    Test

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Interfaz</title>
+</head>
+<body>
+	<h1>TwitLight</h1>
+	<?php
+	if (isset($_GET["twit"])){
+		echo "Twit!";
+
+	}
+
+	?>
+</body>
+</html>
