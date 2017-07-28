b1219b411cbd805025b393633490491305c2d73a
AbdalaGabriel <g.abdala.04@gmail.com>
2017-07-27 23:47:46 -0300

    tw2
    s

--- tw/index2.php
+++ tw/index2.php
@@ -9,34 +9,18 @@ $access_token = "796018368100888576-VxLJbv2peY7nU802GnGiuAoEfBmlUZp";
 $access_token_secret = "83Zj1bobqBqXFIDknSsoLyK1i0FyERpSi8b8H4Z35APre";
 
 $twitter = new TwitterOAuth($consumer_key,$consumer_secret,$access_token,$access_token_secret);
+if(isset($_GET)){
+	$hash = $_GET["tw"];
+}
 
-$tweets = $twitter->get('https://api.twitter.com/1.1/search/tweets.json??l=es&q=%amor&src=typd&count=20&lang=es&result_type=recent');
+$tweets = $twitter->get('https://api.twitter.com/1.1/search/tweets.json??l=es&q=%'.$hash.'&src=typd&count=1&lang=es&result_type=recent');
 
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <title>Twitter API SEARCH</title>
-</head>
-<body>
-
-
-<?php 
 
 foreach ($tweets->statuses as $key => $tweet) {
+
+
+    echo $tweet->text;
+}
+
  ?>
-
-    <div class="twitContainer">
-    <a target="_blank" href="https://twitter.com/gabdala/status/<?php echo $tweet->id_str ?>">
-	    <img src="<?=$tweet->user->profile_image_url;?>" />
-	    <p><?=$tweet->text ?></p>
-
-    </a>
-    <p><?=$tweet->created_at ?></p>
-    </div>
-<?php } ?>
   
-
-</body>
-</html>
